df9bd408e1224726fabc008dff80bf83c79f6a9b
ktodi <todi.kunal0@gmail.com>
2016-09-11 15:23:51 +0530

    master changes from inspire

--- generate_Rank.cpp
+++ generate_Rank.cpp
@@ -1,4 +1,14 @@
-// on other branch
+//Here two rank matrices are generated:-
+
+//1) Rank all robots for each task based on their cost.
+//The least cost robot is given 1st rank and next is given 2nd rank and so on.
+//The highest cost robot is given last rank.
+//This ranking is performed for each task.
+
+//2)  Rank all tasks for each robot based on their cost.
+//The least cost task is given 1st rank and next is given 2nd rank and so on.
+//The highest cost task is given last rank.
+//This ranking is performed for each Robot.
 
 #include <iostream>
 #include <map>
@@ -8,8 +18,11 @@ double** input;
 int n;
 int** rob_rank;
 int** task_rank;
-
+//Here we are using map to sort cost and ranking them.
 map<double,int> sorted;
+//This function genrates Rob_rank ,ranking of robots for each task
+//for each task here the Robot Robot_Id is stored as value with its cost as key.
+//The sorted map is then iterated and corresponding Task column stores the ranking i.e. the values stored in map.
 void make_robot_rank()
 {
 	for(int i=0;i<n;i++)
@@ -27,7 +40,9 @@ void make_robot_rank()
 		sorted.clear();
 	}
 }
-
+//This function genrates Task_rank ,ranking of tasks for each robot
+//for each robot here the task's Task_Id is stored as value with its cost as key.
+//The sorted map is then iterated and corresponding Robot row stores the ranking i.e. the values stored in map.
 void make_task_rank()
 {
 	for(int i=0;i<n;i++)
@@ -40,14 +55,15 @@ void make_task_rank()
 		for(map<double,int> :: iterator it=sorted.begin();it!=sorted.end();it++)
 		{
 			task_rank[i][z]=it->second;
-			//cout<<it->second<<" ";
 			z++;
 
 		}
 		sorted.clear();
-		//cout<<endl;
 	}
 }
+
+//This will print the Task rank and Robot rank in a file.
+//So this output generated here can be used for future reference.
 void print_rank()
 {
 	for(int i=0;i<n;i++)
@@ -77,10 +93,17 @@ void print_rank()
 		cout<<endl;
 	}
 }
+
 int main()
 {
 	cin>>n;
 	double x;
+
+	//Here we are first reading the original matrix in input.
+	//Then 2 rank matrices are genrated viz.
+	//Rob_rank stores ranking based on robots where robots are ranked for each task.
+	//And, Task_rank stores ranking based on tasks where tasks are ranked for each robot.
+
 	input=(double**)malloc(n*sizeof(double*));
 	for(int i=0;i<n;i++)
 	{
